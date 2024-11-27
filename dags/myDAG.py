897a60c6d8a85648386f8bb25ea23f329ccfae06
SherwinDesouza <130744216+SherwinDesouza@users.noreply.github.com>
2024-11-27 17:00:06 +0500

    Added DVC push commands in airflow

--- dags/myDAG.py
+++ dags/myDAG.py
@@ -1,21 +1,21 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
+from airflow.operators.bash import BashOperator
+
 from preprocess_data import processing  
 from data_collection import fetch_weather_data
-
 default_args = {
     'owner': 'airflow',
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    'start_date': datetime(2024, 11, 26),
+    'start_date': datetime(2024, 11, 27),
 }
 
-# Define the DAG
 with DAG(
     'weather_pipeline',
     default_args=default_args,
-    schedule_interval=None,  # Manual execution
+    schedule_interval=None,  
     catchup=False,
 ) as dag:
 
@@ -31,4 +31,17 @@ with DAG(
         python_callable=processing, 
     )
 
-    collect_data_task >> preprocess_data_task
+    DVCWalaKaam = BashOperator(
+        task_id="DVCWalaKaam",
+        bash_command=(
+            'dvc add dags/raw_data.csv dags/processed_data.csv && '
+            'dvc push'
+        ),
+        retries=3,
+        execution_timeout=timedelta(minutes=15),
+    )
+    
+
+
+collect_data_task >> preprocess_data_task >> DVCWalaKaam
+
