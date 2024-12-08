c39cd5899ab0f5db8f958ad820858b6c29f53cc3
SherwinDesouza <130744216+SherwinDesouza@users.noreply.github.com>
2024-12-08 23:22:42 +0500

    FINAL PRE PROCESSINGG FILEEEEE

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,41 +1,17 @@
-import requests
 import pandas as pd
-from datetime import datetime
-import time
+import numpy as np
+import os
+
+def processing():
+    df = pd.read_csv(r'./raw_data.csv')
+
+    # Normalize the data
+    df["temperature"] = np.abs((df["temperature"] - df["temperature"].mean()) / df["temperature"].std())
+    df["wind_speed"] = np.abs((df["wind_speed"] - df["wind_speed"].mean()) / df["wind_speed"].std())
+    df["humidity"] = np.abs((df["humidity"] - df["humidity"].mean()) / df["humidity"].std())
+
+    # Save the processed data
+    df.to_csv('processed_data.csv', index=False)
 
 
-from datetime import datetime
-
-API_KEY = "API_KEY" # Add your API key here
-CITY = "City Name" # Add your city name here
-URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
-weather_data_list = []
-def fetch_weather_data():
-    count =0
-    while count<500:
-        response = requests.get(URL)
-        if response.status_code == 200:
-            data = response.json()
-            weather_data = {
-                "temperature": data["main"]["temp"],
-                "humidity": data["main"]["humidity"],
-                "wind_speed": data["wind"]["speed"],
-                "weather_condition": data["weather"][0]["description"],
-                "date": datetime.now().strftime("%Y-%m-%d"),
-                "time": datetime.now().strftime("%H:%M:%S"),
-            }
-            weather_data_list.append(weather_data)
-            df = pd.DataFrame(weather_data_list)
-            df.to_csv("raw_data.csv", index=False)
-            #print(f"Data collected: {weather_data}")  
-
-            #df = pd.DataFrame(weather_data_list)
-            #df.to_csv("weather_data.csv", index=False)
-        else:
-            print(f"Error: {response.status_code}")
-        
-        #time.sleep(120)
-        count+=1
-        #print("Data count: ",count)
-
-fetch_weather_data()
+processing()    
