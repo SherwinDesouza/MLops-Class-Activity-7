be33406318cf8a1ee9cdd22b8ec7c9a65d07c2d5
SherwinDesouza <130744216+SherwinDesouza@users.noreply.github.com>
2024-12-08 23:21:46 +0500

    FINAL DATA COLLECTION FILEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- data_collection.py
+++ data_collection.py
@@ -6,6 +6,36 @@ import time
 
 from datetime import datetime
 
-current_dateTime = datetime.now()
+API_KEY = "API_KEY" # Add your API key here
+CITY = "City Name" # Add your city name here
+URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+weather_data_list = []
+def fetch_weather_data():
+    count =0
+    while count<500:
+        response = requests.get(URL)
+        if response.status_code == 200:
+            data = response.json()
+            weather_data = {
+                "temperature": data["main"]["temp"],
+                "humidity": data["main"]["humidity"],
+                "wind_speed": data["wind"]["speed"],
+                "weather_condition": data["weather"][0]["description"],
+                "date": datetime.now().strftime("%Y-%m-%d"),
+                "time": datetime.now().strftime("%H:%M:%S"),
+            }
+            weather_data_list.append(weather_data)
+            df = pd.DataFrame(weather_data_list)
+            df.to_csv("raw_data.csv", index=False)
+            #print(f"Data collected: {weather_data}")  
 
-print(current_dateTime)
+            #df = pd.DataFrame(weather_data_list)
+            #df.to_csv("weather_data.csv", index=False)
+        else:
+            print(f"Error: {response.status_code}")
+        
+        #time.sleep(120)
+        count+=1
+        #print("Data count: ",count)
+
+fetch_weather_data()
